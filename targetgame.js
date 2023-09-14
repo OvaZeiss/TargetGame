8cf986a0b73293ac673dd678beb4462c96d37c34
OvaZeiss <ovidiu.oprea@zeiss.com>
2023-09-14 09:23:27 +0300

    define functions to draw target on canvas

--- targetgame.js
+++ targetgame.js
@@ -1,3 +1,6 @@
+//define the size of the target game canvas
+const WIDTH = 800, HEIGHT = 600;
+
 function drawCircle(x, y, z, color, context) {
     context.beginPath();
     context.arc(x, y, z, 0, Math.PI * 2, true);
--- targetgame.ts
+++ targetgame.ts
@@ -1,6 +1,34 @@
-function drawCircle(x:number, y:number, z: size, color: string, context: object){
+//define the size of the target game canvas
+const WIDTH:number = 800, HEIGHT:number = 600
+//define Target's colors
+const TargetColor1:string = "white", TargetColor2:string = "red", BackgroundColor:string = "black"
+
+type Context = CanvasRenderingContext2D;
+
+//define a function that draws a circle
+function drawCircle(x:number, y:number, z: number, color: string, context: Context){
     context.beginPath();
     context.arc(x, y, z, 0, Math.PI * 2, true);
     context.fillStyle = color;
     context.fill();  
 };
+//function taht draws a target
+function drawTarget(context: Context){
+	const x:number = Math.random()*600 + 60;
+    const y:number = Math.random()*400 + 60;
+    drawCircle(x, y, 65, TargetColor1, context);
+    drawCircle(x, y, 50, TargetColor2, context);
+    drawCircle(x, y, 35, TargetColor1, context);
+    drawCircle(x, y, 20, TargetColor2, context);
+}
+
+//
+function main() {
+    const myCanvas = document.getElementById("canvas");
+    if ((myCanvas as HTMLCanvasElement).getContext) {
+        const ctx = (myCanvas as HTMLCanvasElement).getContext("2d");
+        ctx.fillStyle = BackgroundColor;
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        drawTarget(ctx);
+    }
+}
