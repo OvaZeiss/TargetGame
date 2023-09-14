a5ea1e6645eb97c9d254ba4b4cd81ab9ab2c2dfc
OvaZeiss <ovidiu.oprea@zeiss.com>
2023-09-14 20:17:52 +0300

    salvarea de joi seara

--- targetgame.js
+++ targetgame.js
@@ -1,6 +1,44 @@
 //define the size of the target game canvas
-const WIDTH = 800, HEIGHT = 600;
-
+var WIDTH = 800, HEIGHT = 600;
+//define Target's colors
+var TargetColor2 = "white", TargetColor1 = "red", BackgroundColor = "black";
+//Define a Target class
+var Target = /** @class */ (function () {
+    function Target(x, y) {
+        this.grothRate = 0.2;
+        this.maxSize = 30;
+        this.size = 0;
+        this.grow = true;
+        this.x = x;
+        this.y = y;
+    }
+    Target.prototype.update = function () {
+        if (this.size + this.grothRate >= this.maxSize) {
+            this.grow = false;
+        }
+        if (this.grow) {
+            this.size += this.grothRate;
+        }
+        else {
+            this.size -= this.grothRate;
+        }
+    };
+    Target.prototype.collide = function (x, y) {
+        var distance = Math.sqrt(Math.pow((this.x - x), 2) + Math.pow((this.y - y), 2));
+        return distance <= this.size;
+    };
+    //function that draws a target
+    Target.prototype.drawTarget = function (context) {
+        //const x:number = Math.random()*600 + 30;
+        //const y:number = Math.random()*400 + 30;
+        drawCircle(this.x, this.y, this.size * 1.2, TargetColor1, context);
+        drawCircle(this.x, this.y, this.size, TargetColor2, context);
+        drawCircle(this.x, this.y, this.size * 0.8, TargetColor1, context);
+        drawCircle(this.x, this.y, this.size * 0.6, TargetColor2, context);
+    };
+    return Target;
+}());
+//define a function that draws a circle
 function drawCircle(x, y, z, color, context) {
     context.beginPath();
     context.arc(x, y, z, 0, Math.PI * 2, true);
@@ -8,3 +46,88 @@ function drawCircle(x, y, z, color, context) {
     context.fill();
 }
 ;
+// change
+function main() {
+    var targets = [];
+    //clock = pygame.time.Clock()
+    var target_pressed = 0;
+    var clicks = 0;
+    var start_time = new Date(Date.now());
+    var misses = 0;
+    var run = true;
+    var click = false;
+    var mousePosX, mousePosY;
+    var _loop_1 = function () {
+        //clock.tick(60)
+        click = false;
+        //mouse_position = pygame.mouse.get_pos()
+        //let diff = (Date.now() - start_time);
+        //let elapsed_time = Math.floor((diff % (1000 * 60)) / 1000);
+        var myTarget = new Target(Math.random() * 600 + 30, Math.random() * 400 + 30);
+        var myCanvas = document.querySelector("#canvas1");
+        if (myCanvas.getContext) {
+            var ctx_1 = myCanvas.getContext("2d");
+            //ctx.fillStyle = "rgb(0,25,40)";
+            //ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            var i = 0;
+            while (i < 200) {
+                i += 1;
+                setTimeout(function () {
+                    myTarget.update();
+                    myTarget.drawTarget(ctx_1);
+                }, 900);
+            }
+            ;
+        }
+        return "break";
+    };
+    while (run) {
+        var state_1 = _loop_1();
+        if (state_1 === "break")
+            break;
+    }
+    ;
+    /*    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING + TOP_BAR_HEIGHT, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+                print("mouse down click = ", clicks)
+
+        for target in targets:
+            target.update()
+            if target.size <= 0:
+                targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_position):
+                targets.remove(target)
+                target_pressed += 1
+                print(target_pressed)
+
+        end_time = elapsed_time
+        if misses >= LIVES:
+            run = False
+            end_screen(WIN, end_time, target_pressed, clicks)
+
+        draw(WIN, targets)
+        draw_top_bar(WIN, elapsed_time, target_pressed, misses)
+
+        pygame.display.update()
+
+    pygame.quit()   */
+    //   const myCanvas = document.querySelector("#canvas1");
+    //   myCanvas.addEventListener("mousemove", (event) => {
+    //       mousePosX = event.offsetX;
+    //       mousePosY = event.offsetY;
+    //     });
+}
--- targetgame.ts
+++ targetgame.ts
@@ -1,9 +1,52 @@
 //define the size of the target game canvas
 const WIDTH:number = 800, HEIGHT:number = 600
 //define Target's colors
-const TargetColor1:string = "white", TargetColor2:string = "red", BackgroundColor:string = "black"
+const TargetColor2:string = "white", TargetColor1:string = "red", BackgroundColor:string = "black"
 
 type Context = CanvasRenderingContext2D;
+
+//Define a Target class
+class Target{
+    x:number;
+    y:number;
+    grothRate: number = 0.2;
+    maxSize:number = 30;
+    size:number = 0;
+    grow:boolean = true;
+
+    constructor(x:number, y:number){
+        this.x = x;
+        this.y = y;
+    }
+
+    update(): void{
+        if( this.size + this.grothRate >= this.maxSize){
+            this.grow = false;
+        }
+        if(this.grow){
+            this.size += this.grothRate;} else 
+            {
+                this.size -= this.grothRate;
+        }
+    }
+
+    collide(x: number, y:number): boolean{
+        const distance = Math.sqrt((this.x - x)**2 + (this.y - y)**2)
+        return distance <= this.size
+    }
+
+    //function that draws a target
+    drawTarget(context: Context){
+        //const x:number = Math.random()*600 + 30;
+        //const y:number = Math.random()*400 + 30;
+        drawCircle(this.x, this.y, this.size * 1.2, TargetColor1, context);
+        drawCircle(this.x, this.y, this.size, TargetColor2, context);
+        drawCircle(this.x, this.y, this.size * 0.8, TargetColor1, context);
+        drawCircle(this.x, this.y, this.size * 0.6, TargetColor2, context);
+    }    
+}
+
+
 
 //define a function that draws a circle
 function drawCircle(x:number, y:number, z: number, color: string, context: Context){
@@ -12,23 +55,87 @@ function drawCircle(x:number, y:number, z: number, color: string, context: Conte
     context.fillStyle = color;
     context.fill();  
 };
-//function taht draws a target
-function drawTarget(context: Context){
-	const x:number = Math.random()*600 + 60;
-    const y:number = Math.random()*400 + 60;
-    drawCircle(x, y, 65, TargetColor1, context);
-    drawCircle(x, y, 50, TargetColor2, context);
-    drawCircle(x, y, 35, TargetColor1, context);
-    drawCircle(x, y, 20, TargetColor2, context);
-}
 
-//
+// change
 function main() {
-    const myCanvas = document.getElementById("canvas");
-    if ((myCanvas as HTMLCanvasElement).getContext) {
-        const ctx = (myCanvas as HTMLCanvasElement).getContext("2d");
-        ctx.fillStyle = BackgroundColor;
-        ctx.fillRect(0, 0, WIDTH, HEIGHT);
-        drawTarget(ctx);
-    }
+    let targets: Target[] = [];
+    //clock = pygame.time.Clock()
+    let target_pressed:number = 0
+    let clicks:number = 0
+    let start_time:Date = new Date(Date.now());
+    let misses:number = 0  ;
+    let run = true;
+    let click: boolean = false;
+    let mousePosX:number , mousePosY:number;
+
+    while (run) {
+        //clock.tick(60)
+        click = false;
+        //mouse_position = pygame.mouse.get_pos()
+        //let diff = (Date.now() - start_time);
+        //let elapsed_time = Math.floor((diff % (1000 * 60)) / 1000);
+       
+        let myTarget: Target = new Target(Math.random()*600 + 30, Math.random()*400 + 30);
+        const myCanvas = document.querySelector("#canvas1");        
+        if ((myCanvas as HTMLCanvasElement).getContext) {
+            const ctx = (myCanvas as HTMLCanvasElement).getContext("2d");
+            //ctx.fillStyle = "rgb(0,25,40)";
+            //ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            let i:number = 0;
+            while( i < 200){
+                i += 1;
+                setTimeout(() => {
+                    myTarget.update();
+                    myTarget.drawTarget(ctx);
+                  }, 900);   
+            } ;                                       
+        } 
+
+        break; 
+    };
+    /*    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING + TOP_BAR_HEIGHT, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+                print("mouse down click = ", clicks)
+
+        for target in targets:
+            target.update()
+            if target.size <= 0:
+                targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_position):
+                targets.remove(target)
+                target_pressed += 1
+                print(target_pressed)
+
+        end_time = elapsed_time
+        if misses >= LIVES:
+            run = False
+            end_screen(WIN, end_time, target_pressed, clicks)
+
+        draw(WIN, targets)
+        draw_top_bar(WIN, elapsed_time, target_pressed, misses)
+
+        pygame.display.update()
+
+    pygame.quit()   */ 
+    
+ //   const myCanvas = document.querySelector("#canvas1");
+ //   myCanvas.addEventListener("mousemove", (event) => {
+ //       mousePosX = event.offsetX;
+ //       mousePosY = event.offsetY;
+ //     });
+
 }
